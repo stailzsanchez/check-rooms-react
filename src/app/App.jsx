7efd8ba1bd083c70bbd977d7a2867947e34a996b
stailzsanchez <stailzsanchez@gmail.com>
2024-10-08 02:40:10 +0300

    tg ready

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+import { useTelegram } from '../shared/telegram/useTelegram';
 import { AppRouter } from './providers/router/AppRouter';
 import './style/index.css';
 
 export const App = () => {
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
     <div className="app">
       <AppRouter />
