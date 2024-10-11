ff4c209e54f43fecf44f7331d01e5d4d84c0d0af
stailz <stailzsanchez@gmail.com>
2024-10-11 19:39:00 +0300

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+      name: 'Название вашего приложения',
+      short_name: 'Краткое название',
+      description: 'Описание вашего приложения',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'check192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'check512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       "@/": "/src",
@@ -13,5 +37,6 @@ export default defineConfig({
       assets: "/src/shared/assets",
       pages: "/src/pages",
     },
-  }
+  },
+
 })
